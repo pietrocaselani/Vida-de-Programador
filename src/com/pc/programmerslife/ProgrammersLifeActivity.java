4a99f4f70a5debb628b53743e783be7676912a71
Pietro Caselani <pc1992@gmail.com>
2012-03-16 19:33:53 -0300

    First commit

---
+++ src/com/pc/programmerslife/ProgrammersLifeActivity.java
@@ -0,0 +1,13 @@
+package com.pc.programmerslife;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProgrammersLifeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
