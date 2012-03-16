f3f7f4bcb760f7df8c2282aaf5727a256c72ed1e
Pietro Caselani <pc1992@gmail.com>
2012-03-16 19:42:12 -0300

    Added ActionBarSherlock library

--- src/com/pc/programmerslife/ProgrammersLifeActivity.java
+++ src/com/pc/programmerslife/ProgrammersLifeActivity.java
@@ -1,13 +1,14 @@
 package com.pc.programmerslife;
 
-import android.app.Activity;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import android.os.Bundle;
 
-public class ProgrammersLifeActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class ProgrammersLifeActivity extends SherlockFragmentActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
