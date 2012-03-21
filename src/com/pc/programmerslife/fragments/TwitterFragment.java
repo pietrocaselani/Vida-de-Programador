92c394a70cdc089026f4056bcac8671ca8a22dcb
Pietro Caselani <pc1992@gmail.com>
2012-03-21 20:11:45 -0300

    Removed action bar menu from TwitterFragment

--- src/com/pc/programmerslife/fragments/TwitterFragment.java
+++ src/com/pc/programmerslife/fragments/TwitterFragment.java
@@ -1,14 +1,8 @@
 package com.pc.programmerslife.fragments;
 
-
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockListFragment;
-import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuInflater;
-import com.actionbarsherlock.view.MenuItem;
-import com.pc.programmerslife.R;
 
 public class TwitterFragment extends SherlockListFragment {
 	
@@ -17,20 +11,5 @@ public class TwitterFragment extends SherlockListFragment {
 		super.onActivityCreated(savedInstanceState);
 		
 		setHasOptionsMenu(true);
-		
-		
-	}
-	
-	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		inflater.inflate(R.menu.twitter_fragment_menu, menu);
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == R.id.twitter_menu_actions) {
-			Toast.makeText(getSherlockActivity(), "Clicou no item do twitter - Share", Toast.LENGTH_LONG).show();
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
