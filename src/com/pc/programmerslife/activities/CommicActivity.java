20639a42555f6a18d33bab5b8f2a38ccf60279bf
Pietro Caselani <pc1992@gmail.com>
2012-03-21 20:10:38 -0300

    Visual adjustments; Action bar logo now is back button

--- src/com/pc/programmerslife/activities/CommicActivity.java
+++ src/com/pc/programmerslife/activities/CommicActivity.java
@@ -3,7 +3,9 @@ package com.pc.programmerslife.activities;
 import android.os.Bundle;
 import android.webkit.WebView;
 
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.MenuItem;
 import com.pc.programmerslife.Commic;
 import com.pc.programmerslife.R;
 
@@ -16,12 +18,25 @@ public class CommicActivity extends SherlockActivity {
 		
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
-			Object o = extras.getParcelable(Commic.EXTRA_COMMIC);
-			Commic commic = (Commic) o;
+			Commic commic = (Commic) extras.getParcelable(Commic.EXTRA_COMMIC);
 			if (commic != null) {
+				ActionBar actionBar = getSupportActionBar();
+				actionBar.setDisplayShowHomeEnabled(true);
+				actionBar.setHomeButtonEnabled(true);
+				actionBar.setTitle(commic.getTitle());
+				
 				WebView webView = (WebView) findViewById(R.id.commicActivity_webView);
 				webView.loadUrl(commic.getPath());
 			}
 		}
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
