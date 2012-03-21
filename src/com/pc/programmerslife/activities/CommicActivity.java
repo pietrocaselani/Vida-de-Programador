fee282ef76d57aec590c975e6d1d1451bfc25138
Pietro Caselani <pc1992@gmail.com>
2012-03-21 20:40:14 -0300

    Share commic link enable

--- src/com/pc/programmerslife/activities/CommicActivity.java
+++ src/com/pc/programmerslife/activities/CommicActivity.java
@@ -1,15 +1,18 @@
 package com.pc.programmerslife.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.pc.programmerslife.Commic;
 import com.pc.programmerslife.R;
 
 public class CommicActivity extends SherlockActivity {
+	private Commic commic;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,7 @@ public class CommicActivity extends SherlockActivity {
 		
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
-			Commic commic = (Commic) extras.getParcelable(Commic.EXTRA_COMMIC);
+			commic = (Commic) extras.getParcelable(Commic.EXTRA_COMMIC);
 			if (commic != null) {
 				ActionBar actionBar = getSupportActionBar();
 				actionBar.setDisplayShowHomeEnabled(true);
@@ -32,10 +35,24 @@ public class CommicActivity extends SherlockActivity {
 	}
 	
 	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.commic_activity_menu, menu);
+		return true;
+	}
+	
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if (item.getItemId() == android.R.id.home) {
 			finish();
 			return true;
+		} else if (item.getItemId() == R.id.commicActivityMenu_share) {
+			Intent shareIntent = new Intent(Intent.ACTION_SEND);
+			shareIntent.putExtra(Intent.EXTRA_TEXT, (commic.getLink() != null) ? commic.getLink() : commic.getPath());
+			shareIntent.setType("text/plain");
+			startActivity(Intent.createChooser(shareIntent, getString(R.string.share_using)));
+			return true;
+		} else if (item.getItemId() == R.id.commicActivityMenu_favorite) {
+			
 		}
 		return super.onOptionsItemSelected(item);
 	}
